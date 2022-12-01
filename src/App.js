ec63e3426907ce4de3e6caf1ecc8af442f237af0
Renan Martins <renanmartins989@gmail.com>
2022-12-01 23:53:58 +0000

    start react-strapi ecommerce

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react'
+import Home from "./Pages/Home/Home"
+import Products from "./Pages/Products/Products"
+import Product from "./Pages/Product/Product";
 
-function App() {
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<Home />
+  },
+  {
+    path:"/products/:id",
+    element:<Products />
+  },
+  {
+    path:"/product/:id",
+    element:<Product />
+  },
+])
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <RouterProvider router={router} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
