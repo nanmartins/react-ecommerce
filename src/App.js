3d716e71e45f27ea0291dced71843b0be27a5031
Renan Martins <renanmartins989@gmail.com>
2022-12-02 00:54:36 +0000

    set body layout, icons import (mui lib), start navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 // import React from 'react'
-import Home from "./Pages/Home/Home"
-import Products from "./Pages/Products/Products"
-import Product from "./Pages/Product/Product";
-
 import {
   createBrowserRouter,
   RouterProvider,
+  Outlet,
 } from "react-router-dom";
+
+import Home from "./Pages/Home/Home"
+import Products from "./Pages/Products/Products"
+import Product from "./Pages/Product/Product";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+
+const Layout = () => {
+  return (
+    <div className="app">
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  )
+}
+
 
 const router = createBrowserRouter([
   {
     path:"/",
-    element:<Home />
-  },
-  {
-    path:"/products/:id",
-    element:<Products />
-  },
-  {
-    path:"/product/:id",
-    element:<Product />
+    element:<Layout />,
+    children: [
+      {
+        path:"/",
+        element:<Home />
+      },
+      {
+        path:"/product/:id",
+        element:<Product />
+      },
+      {
+        path:"/products/:id",
+        element:<Products />
+      },
+    ]
   },
 ])
 
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import SearchIcon from '@mui/icons-material/Search';
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import AddShoppingCartOutlinedIcon from '@mui/icons-material/AddShoppingCartOutlined';
+import LocalGroceryStoreOutlinedIcon from '@mui/icons-material/LocalGroceryStoreOutlined';
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="left"></div>
+        <div className="center"></div>
+        <div className="right"></div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
