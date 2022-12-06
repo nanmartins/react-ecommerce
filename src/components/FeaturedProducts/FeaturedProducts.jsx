9794d0de1417408a10b7fcb6b14d4bb93dda5c6e
Renan Martins <renanmartins989@gmail.com>
2022-12-06 00:42:58 +0000

    card img change

--- src/components/FeaturedProducts/FeaturedProducts.jsx
+++ src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -18,8 +18,8 @@ const FeaturedProducts = ({type}) => {
     },
     {
       id: 2,
-      img: "https://images.pexels.com/photos/1759622/pexels-photo-1759622.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      img2: "https://images.pexels.com/photos/1163194/pexels-photo-1163194.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      img: "https://images.pexels.com/photos/1163194/pexels-photo-1163194.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      img2: "https://images.pexels.com/photos/1759622/pexels-photo-1759622.jpeg?auto=compress&cs=tinysrgb&w=1600",
       title: "Coat",
       isNew: true,
       oldPrice: 19,
