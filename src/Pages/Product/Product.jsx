4bb8cd4616d3f5abfdef8eef99599e5ea05623c1
Renan Martins <renanmartins989@gmail.com>
2022-12-08 02:08:08 +0000

    just small details fixes

--- src/Pages/Product/Product.jsx
+++ src/Pages/Product/Product.jsx
@@ -18,54 +18,54 @@ const Product = () => {
 
   return (
     <>
-    <div className='product'>
-      <div className="left">
-        <div className="images">
-          <img src={images[0]} alt="" onClick={e => setSelectedImg(0)} />
-          <img src={images[1]} alt="" onClick={e => setSelectedImg(1)} />
+      <div className='product'>
+        <div className="left">
+          <div className="images">
+            <img src={images[0]} alt="" onClick={e => setSelectedImg(0)} />
+            <img src={images[1]} alt="" onClick={e => setSelectedImg(1)} />
+          </div>
+          <div className="mainImg">
+            <img src={images[selectedImg]} alt="" />
+          </div>
         </div>
-        <div className="mainImg">
-          <img src={images[selectedImg]} alt="" />
-        </div>
-      </div>
 
-      <div className="right">
-        <h1>Title</h1>
-        <span className='price'>£120</span>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste minus eum tempore. Quo distinctio sint nihil ex repudiandae. Id aliquam facilis pariatur omnis laborum sequi ad impedit hic asperiores voluptatibus!
-        Nostrum, rerum vero! Ducimus voluptas eveniet expedita officia! Mollitia voluptatem rem, minima odit vel nemo laboriosam nihil error illum ipsum porro sequi quia cupiditate ducimus aliquid fugit.</p>
-        <div className="quantity">
-          <button onClick={() => setQuantity((prev) => prev === 1 ? 1 : prev - 1)}>-</button>
-          {quantity}
-          <button onClick={() => setQuantity((prev) => prev + 1)}>+</button>
-        </div>
-        <button className="add">
-          <AddShoppingCartOutlinedIcon /> ADD TO CART
-        </button>
-        <div className="links">
-          <div className="item">
-            <FavoriteBorderIcon /> ADD TO WISH LIST
+        <div className="right">
+          <h1>Animal Print Coat</h1>
+          <span className='price'>£120</span>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste minus eum tempore. Quo distinctio sint nihil ex repudiandae. Id aliquam facilis pariatur omnis laborum sequi ad impedit hic asperiores voluptatibus!
+          Nostrum, rerum vero! Ducimus voluptas eveniet expedita officia! Mollitia voluptatem rem, minima odit vel nemo laboriosam nihil error illum ipsum porro sequi quia cupiditate ducimus aliquid fugit.</p>
+          <div className="quantity">
+            <button onClick={() => setQuantity((prev) => prev === 1 ? 1 : prev - 1)}>-</button>
+            {quantity}
+            <button onClick={() => setQuantity((prev) => prev + 1)}>+</button>
           </div>
-          <div className="item">
-            <ShareIcon /> SHARE
+          <button className="add">
+            <AddShoppingCartOutlinedIcon /> ADD TO CART
+          </button>
+          <div className="links">
+            <div className="item">
+              <FavoriteBorderIcon /> ADD TO WISH LIST
+            </div>
+            <div className="item">
+              <ShareIcon /> SHARE
+            </div>
           </div>
-        </div>
-        <div className="info">
-          <span>Vendor: zStore</span>
-          <span>Product Type: Coat</span>
-          <span>Tag: Coat, Women, Winter</span>
-        </div>
-        <hr />
-        <div className="info">
-          <span>DESCRIPTION</span>
+          <div className="info">
+            <span>Vendor: zStore</span>
+            <span>Product Type: Coat</span>
+            <span>Tag: Coat, Women, Winter</span>
+          </div>
           <hr />
-          <span>ADDITIONAL INFORMATION</span>
-          <hr />
-          <span>FAQ</span>
+          <div className="info">
+            <span>DESCRIPTION</span>
+            <hr />
+            <span>ADDITIONAL INFORMATION</span>
+            <hr />
+            <span>FAQ</span>
+          </div>
         </div>
       </div>
-    </div>
-    <Contact />
+      <Contact />
     </>
   )
 }
--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -13,48 +13,48 @@ const Products = () => {
 
   return (
     <>
-    <div className='products'>
-      <div className="left">
-        <div className="filterItem">
-          <h2>Product Categories</h2>
-          <div className="inputItem">
-            <input type="checkbox" id="1" value={1} />
-            <label htmlFor="1">Women</label>
+      <div className='products'>
+        <div className="left">
+          <div className="filterItem">
+            <h2>Product Categories</h2>
+            <div className="inputItem">
+              <input type="checkbox" id="1" value={1} />
+              <label htmlFor="1">Women</label>
+            </div>
+            <div className="inputItem">
+              <input type="checkbox" id="2" value={2} />
+              <label htmlFor="2">Men</label>
+            </div>
+            <div className="inputItem">
+              <input type="checkbox" id="3" value={3} />
+              <label htmlFor="3">Kids</label>
+            </div>
           </div>
-          <div className="inputItem">
-            <input type="checkbox" id="2" value={2} />
-            <label htmlFor="2">Men</label>
+          <div className="filterItem">
+            <h2>Filter by price</h2>
+            <span>1</span>
+              <input type="range" min={1} max={1000} onChange={(e) => setMaxPrice(e.target.value)} />
+            <span>{maxPrice}</span>
           </div>
-          <div className="inputItem">
-            <input type="checkbox" id="3" value={3} />
-            <label htmlFor="3">Kids</label>
+          <div className="filterItem">
+            <h2>Sort by</h2>
+            <div className="inputItem">
+              <input type="radio" id="asc" value="asc" name="price" onChange={e => setSort("asc")} />
+              <label htmlFor='asc'>Price (Lowest first)</label>
+            </div>
+            <div className="inputItem">
+              <input type="radio" id="desc" value="desc" name="price" onChange={e => setSort("asc")} />
+              <label htmlFor='desc'>Price (Heighest first)</label>
+            </div>
           </div>
         </div>
-        <div className="filterItem">
-          <h2>Filter by price</h2>
-          <span>1</span>
-            <input type="range" min={1} max={1000} onChange={(e) => setMaxPrice(e.target.value)} />
-          <span>{maxPrice}</span>
-        </div>
-        <div className="filterItem">
-          <h2>Sort by</h2>
-          <div className="inputItem">
-            <input type="radio" id="asc" value="asc" name="price" onChange={e => setSort("asc")} />
-            <label htmlFor='asc'>Price (Lowest first)</label>
-          </div>
-          <div className="inputItem">
-            <input type="radio" id="desc" value="desc" name="price" onChange={e => setSort("asc")} />
-            <label htmlFor='desc'>Price (Heighest first)</label>
-          </div>
-        </div>
-      </div>
 
-      <div className="right">
-        <img className='catImg' src="https://images.pexels.com/photos/206430/pexels-photo-206430.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
-        <List catId={catId} maxPrice={maxPrice} sort={sort} />
+        <div className="right">
+          <img className='catImg' src="https://images.pexels.com/photos/206430/pexels-photo-206430.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+          <List catId={catId} maxPrice={maxPrice} sort={sort} />
+        </div>
       </div>
-    </div>
-    <Contact />
+      <Contact />
     </>
   )
 }
